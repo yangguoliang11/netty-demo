7b581b71da72803a63daa139fc43b131e78cf8c8
guoliang <yanggl@jvtd.cn>
2020-12-29 20:31:27 +0800

    多线程尝试

--- src/main/java/cn/jvtd/netty/demo/DemoApplication.java
+++ src/main/java/cn/jvtd/netty/demo/DemoApplication.java
@@ -1,7 +1,13 @@
 package cn.jvtd.netty.demo;
 
+import cn.jvtd.netty.demo.thread.MyThread;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Aaronglyang
@@ -9,11 +15,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(DemoApplication.class, args);
+        String str = LocalDateTime.now().toString();
 
-        
-        System.out.println("testok");
+
+        System.out.println(str);
+
+        //验证一下Optional
+        List<String> list = new ArrayList<>();
+        list.add("guoliang");
+        list.add(null);
+        list.add(null);
+        String str2 = Optional.ofNullable(list.get(1)).orElse("aa");
+
+
+        System.out.println("str2:"+str2);
+
+        MyThread myThread = new MyThread();
+        myThread.start();
+        System.out.println("线程结束！！");
+
+        for(Integer i = 0; i<10; i++){
+            int time = (int)(Math.random()*1000);
+            Thread.sleep(time);
+            System.out.println("main="+Thread.currentThread().getName());
+
+        }
+
+
     }
 
 }
