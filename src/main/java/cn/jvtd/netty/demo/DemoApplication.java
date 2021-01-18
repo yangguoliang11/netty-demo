f6832ac0320d9753bba7f261dff979f36fad2164
guoliang <yanggl@jvtd.cn>
2021-01-18 20:20:46 +0800

    线程池

--- src/main/java/cn/jvtd/netty/demo/DemoApplication.java
+++ src/main/java/cn/jvtd/netty/demo/DemoApplication.java
@@ -2,9 +2,11 @@ package cn.jvtd.netty.demo;
 
 import cn.jvtd.netty.demo.controller.Plusplus;
 import cn.jvtd.netty.demo.controller.ThreadSynchronized;
-import cn.jvtd.netty.demo.controller.TicketWindow;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.*;
 
 /**
  * @author Aaronglyang
@@ -24,18 +26,43 @@ public class DemoApplication {
 //
 //        TicketWindow ticketWindow3 = new TicketWindow("3hao");
 //        ticketWindow3.start();
-        Plusplus plusplus = new Plusplus();
-        ThreadSynchronized ts1 = new ThreadSynchronized("一号窗口",plusplus);
-        ts1.start();
-        ThreadSynchronized ts2 = new ThreadSynchronized("二号窗口",plusplus);
-        ts2.start();
-        ThreadSynchronized ts3 = new ThreadSynchronized("三号窗口",plusplus);
-        ts3.start();
-        ThreadSynchronized ts4 = new ThreadSynchronized("四号窗口",plusplus);
-        ts4.start();
-        ThreadSynchronized ts5 = new ThreadSynchronized("五号窗口",plusplus);
-        ts5.start();
 
+//        Plusplus plusplus = new Plusplus();
+//        ThreadSynchronized ts1 = new ThreadSynchronized("一号窗口",plusplus);
+//        ts1.start();
+//        ThreadSynchronized ts2 = new ThreadSynchronized("二号窗口",plusplus);
+//        ts2.start();
+//        ThreadSynchronized ts3 = new ThreadSynchronized("三号窗口",plusplus);
+//        ts3.start();
+//        ThreadSynchronized ts4 = new ThreadSynchronized("四号窗口",plusplus);
+//        ts4.start();
+//        ThreadSynchronized ts5 = new ThreadSynchronized("五号窗口",plusplus);
+//        ts5.start();
+    //线程池开启
+        long startTime = System.currentTimeMillis();
+        //获取开始时间
+        ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("gateway-dispatcher-%d").build();
+
+        ExecutorService esPool =new ThreadPoolExecutor(5,6,0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingDeque<>(6),threadFactory,new ThreadPoolExecutor.AbortPolicy());
+        Plusplus plusplus = new Plusplus();
+        ThreadSynchronized ts1 = new ThreadSynchronized("测试1",plusplus);
+        esPool.execute(ts1);
+        ThreadSynchronized ts2 = new ThreadSynchronized("测试2",plusplus);
+        esPool.execute(ts2);
+        ThreadSynchronized ts3 = new ThreadSynchronized("测试3",plusplus);
+        esPool.execute(ts3);
+        ThreadSynchronized ts4 = new ThreadSynchronized("测试4",plusplus);
+        esPool.execute(ts4);
+        ThreadSynchronized ts5 = new ThreadSynchronized("测试5",plusplus);
+        esPool.execute(ts5);
+        ThreadSynchronized ts6 = new ThreadSynchronized("测试6",plusplus);
+        esPool.execute(ts6);
+        long endTime = System.currentTimeMillis();
+        //获取结束时间
+        System.out.println("程序运行时间：" + (endTime - startTime) + "ms");
+        //输出程序运行时间
+        esPool.shutdown();
     }
 
 }
---
+++ src/main/java/cn/jvtd/netty/demo/controller/ThreadRunable.java
@@ -0,0 +1,20 @@
+package cn.jvtd.netty.demo.controller;
+
+public class ThreadRunable implements Runnable{
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+
+    }
+}
